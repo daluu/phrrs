e3e484d64dc00b8b663ed4bb2e61e23f19cc8c7e
Jean-Pierre Lambert <jeanpi.lambert@gmail.com>
2016-04-30 15:41:10 -0700

    Made demo data easilly available

--- robotremote/RobotRemoteServer.php
+++ robotremote/RobotRemoteServer.php
@@ -124,37 +124,3 @@ Content-Length: '.$contentLength."\r\n"
 	}
 
 }
-
-class DemoRequests {
-
-	private $requests = array(
-		'<?xml version="1.0"?>
-		<methodCall>
-		   <methodName>get_keyword_names</methodName>
-		   <params>
-		      </params>
-		   </methodCall>',
-		'<?xml version="1.0"?>
-		<methodCall>
-		   <methodName>stop_remote_server</methodName>
-		   <params>
-		      </params>
-		   </methodCall>'
-	);
-	private $requestsIdx = 0;
-
-	public function get() {
-		$request = $this->requests[$this->requestsIdx++];
-		echo('Request: '.$request."\n\n");
-		return $request;
-	}
-
-}
-
-class DemoResponses {
-
-	public function add($response) {
-		echo('Response: '.$response."\n\n");
-	}
-
-}
